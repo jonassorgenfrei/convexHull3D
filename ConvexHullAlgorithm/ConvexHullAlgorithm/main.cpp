04f44f73a4321335085b551b229f765c0ba91d08
Jonas Sorgenfrei <Jonas_sorgenfrei@yahoo.de>
2018-10-28 16:48:15 +0100

    Main Message

--- ConvexHullAlgorithm/ConvexHullAlgorithm/main.cpp
+++ ConvexHullAlgorithm/ConvexHullAlgorithm/main.cpp
@@ -1,8 +1,18 @@
+/*
+ * Sample program for convex hull calculation in 3-Space
+ * 
+ * Part of the seminar talk at FH Wedel 2018
+ * sample program
+ *
+ * @author -	Jonas Sorgenfrei
+ *				Minf101767
+ */
+
 #include <iostream>
 
 
 int main() {
-	std::cout << "Hello World" << std::endl;
+	std::cout << "Convex Hull Program" << std::endl;
 	
 	return 0;
 }
